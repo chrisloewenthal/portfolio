255ceeaabce9d716a20d28792b8fcb4b01ef2e0f
Christopher Loewenthal <chriskloewenthal@gmail.com>
2025-03-17 14:03:13 -0700

    added css file and folder

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 
-
+//gets element with id 'date' and sets the text insite it to a new Date() object
+//that is converted into a more readable string
 document.getElementById('date').innerHTML = new Date().toDateString();
 
 
